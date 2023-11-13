3d827e467186339b2660d19e9d9a8deaad4ffeb0
Jasper Wietses <j.wietses@student.rug.nl>
2023-11-13 12:21:00 +0100

    fig01 produced + restore added

--- scripts/first script.R
+++ scripts/first script.R
@@ -4,11 +4,18 @@ library(renv)
 #restore the versions of the packages used to develop this project
 renv::restore()
 
-install.packages("ggplot2")
-library(ggplot2)
+#remember to use pull in Git tab to get changes from Github
+
+install.packages("tidyverse")
+library(tidyverse)
+
 
 x<-c(1,2,3,4,5)
 y<-c(1,4,3,6,9)
 data<-data.frame(x,y)
 data
 
+fig01<-data|> ggplot(aes(x=x, y=y))+
+                geom_point()+ 
+  geom_smooth(method='lm')
+ggsave("./figures/fig01.png", plot=fig01, width=1920,height=1068,units="px")
