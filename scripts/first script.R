b62b68c58534a617f9f6ed7a4dd4be5776b03bbc
Jasper Wietses <j.wietses@student.rug.nl>
2023-11-13 12:05:34 +0100

    project initialized with renv

--- scripts/first script.R
+++ scripts/first script.R
@@ -1,1 +1,14 @@
 # my first github synced script
+library(renv)
+##renv::init()## to lock versions of library, do it in console
+#restore the versions of the packages used to develop this project
+renv::restore()
+
+install.packages("ggplot2")
+library(ggplot2)
+
+x<-c(1,2,3,4,5)
+y<-c(1,4,3,6,9)
+data<-data.frame(x,y)
+data
+
